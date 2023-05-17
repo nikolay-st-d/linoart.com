20696435a60c9e6fd288848e4bc910ff60356108
Nikolay-St-D <126271396+Nikolay-St-D@users.noreply.github.com>
2023-05-18 00:58:34 +0300

    Minor changes on the activities page

--- activities.php
+++ activities.php
@@ -18,8 +18,7 @@ echo '</div">';
 			<h1>COMPANY ACTIVITIES</h1>
 			<h2>LINOART Ltd. - BULGARIA</h2>
 			<p>We work on many web projects, mainly company websites of hotels and guest houses owners and operators. This work combines photography, graphic design, programming, SEO and promotional services.</p>
-			<p>In 2015 the company launched an on-line shop to sell metal detectors and metal detecting accessories of the Bulgarian brand Golden Mask: <a target="_blank" href="https://www.goldenmaskdetectors.com" title="Golden Mask metal detectors official shop">www.goldenmaskdetectors.com</a> - GMD.</p>
-			<p>Using the company's knowledge in web design, photography and SEO, the GMD shop quickly became the leader in on-line sales of the brand and gained a statute of official Golden Mask reseller. The shop has clients from all over the world, mainly final customers, but also some resellers in the Middle East, China, India, North Africa and South America.</p>
+			<p>In 2015 the company launched an on-line shop to sell metal detectors and metal detecting accessories of the Bulgarian brand Golden Mask: <a target="_blank" href="https://www.goldenmaskdetectors.com" title="Golden Mask metal detectors official shop">www.goldenmaskdetectors.com</a> - GMD. Using the company's knowledge in web design, photography and SEO, the GMD shop quickly became the leader in on-line sales of the brand and gained a statute of official Golden Mask reseller. The shop has clients from all over the world, mainly final customers, but also some resellers in the Middle East, China, India, North Africa and South America.</p>
 		</div>
 		<div class="main_right">
 			<img src="images/top_3.jpg" alt="Linoart Ltd. activities" />
