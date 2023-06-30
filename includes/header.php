67f4ee9d6c207473ca446c86a3af30ebe741afa2
nikolay-st-d <126271396+nikolay-st-d@users.noreply.github.com>
2023-06-30 12:19:09 +0300

    logo adjustment

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <div class="header_container">
 	<div class="header_left">
-		<a href="index.php" title="Linoart Ltd. - Bulgarian company"><img src="images/logo.gif" alt="Linoart" title="Linoart Ltd." /></a>
+		<a href="index.php" title="Linoart Ltd. - Bulgarian company"><img src="images/logo.gif" width="130" height="150" alt="Linoart visual creative studio" title="Linoart Ltd." /></a>
 	</div>
 	<div class="header_right">
 	   <p>Linoart Ltd.<br>Sliven - Bulgaria</p>
