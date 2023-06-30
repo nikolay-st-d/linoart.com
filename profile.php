936913f9d3f7b2a8347e4d901f3e1a76bd299661
nikolay-st-d <126271396+nikolay-st-d@users.noreply.github.com>
2023-06-30 08:52:26 +0300

    styles edited

--- profile.php
+++ profile.php
@@ -18,8 +18,8 @@ echo '</div">';
 			<h1>COMPANY PROFILE</h1>
 			<h2>LINOART Ltd.</h2>
 			<p>Linoart Ltd. was founded in 2005 in the city of Sliven, South-East Bulgaria, as a graphic design and commercial photography studio.</p>
-			<p>The company was registered wit capital of 5000 BGN, that equals 2500 EUR (the minimum capital required by the law is 1 EUR).</p>
-			<p>Linoart is a reputable company, well known in the country for the high quality of the services it provides.</p>
+			<p>The company was registered with capital of 5000 BGN, that equals 2500 EUR (the minimum capital required by the law is 1 EUR).</p>
+			<p>Linoart is a reputable company, well known in the country for the high quality of the services we provides.</p>
 		</div>
 		<div class="main_right">
 			<img src="images/top_2.jpg" alt="Linoart Ltd. - company profile" title="Linoart - Bulgaria" />
